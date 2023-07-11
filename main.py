53d996976de0a43137432c344574da0019e07d87
Acodhw <digbu710@gmail.com>
2023-07-11 17:36:26 +0900

    3 find algorithm made using Python syntax (string, list slice) required by the library

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-dic = {"hello":"World!", "main":"dotC"}
-print(dic["hello"])
-print(dic["main"])
+a = "3213126312587421621537215362185365216352173671735126358214734128753612738421538621438" # value
+
+i = -1
+k = a.find('3') # first find  = -1 check
+
+while not k == -1: # loop until not finded 3
+    i = k + i + 1 # sum and get cut index
+    print("cut index = ", i, " : ", end="") # print index
+    a = a[k+1:len(a)] # cut after '3'
+    print(a) # print cut string
+    k = a.find('3') # find next '3' index
+    
